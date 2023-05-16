2facdab85052bcbf25488b69afe7db790b14fb8a
Courtenay <courtenay@Courtenays-MacBook-Pro.local>
2023-05-16 12:09:47 +0100

    Update private_service_access module as the previously stated version is now longer available to download

--- templates/tfengine/components/resources/compute_networks/main.tf
+++ templates/tfengine/components/resources/compute_networks/main.tf
@@ -143,7 +143,7 @@ resource "google_vpc_access_connector" "{{$resource_name}}_{{.name}}" {
 {{- if has . "cloud_sql_private_service_access"}}
 module "cloud_sql_private_service_access_{{$resource_name}}" {
   source  = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
-  version = "~> 4.5.0"
+  version = "~> 15.0.0"
 
   project_id  = module.project.project_id
   vpc_network = module.{{$resource_name}}.network_name
